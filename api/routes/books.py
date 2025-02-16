df8f05ba8fe81797ed365c44339d70a04740725c
bravevalley <dassyareg@gmail.com>
2025-02-16 11:52:06 +0100

    update: routes

--- api/routes/books.py
+++ api/routes/books.py
@@ -1,4 +1,3 @@
-
 from typing import OrderedDict
 
 from fastapi import APIRouter, status
@@ -48,6 +47,12 @@ async def create_book(book: Book):
 async def get_books() -> OrderedDict[int, Book]:
     return db.get_books()
 
+@router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
+async def get_book(book_id: int) -> Book:
+    book = db.get_book(book_id)
+    if book:
+        return book
+    return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={"detail": "Book not found"})
 
 @router.put("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
 async def update_book(book_id: int, book: Book) -> Book:
@@ -61,4 +66,3 @@ async def update_book(book_id: int, book: Book) -> Book:
 async def delete_book(book_id: int) -> None:
     db.delete_book(book_id)
     return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content=None)
-
